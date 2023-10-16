587bc61a33e9244b3569c88eaeab90a52cf57484
kasperpawlowski <25374117+kasperpawlowski@users.noreply.github.com>
2023-10-16 13:47:25 +0100

    fix: docs

--- src/interfaces/IPriceOracle.sol
+++ src/interfaces/IPriceOracle.sol
@@ -3,26 +3,49 @@
 pragma solidity ^0.8.0;
 
 interface IPriceOracle {
+    /// @notice Returns the name of the price oracle.
     function name() external view returns (string memory);
 
+    /// @notice Returns the quote for a given amount of base asset in quote asset.
+    /// @param amount The amount of base asset.
+    /// @param base The address of the base asset.
+    /// @param quote The address of the quote asset.
+    /// @return out The quote amount in quote asset.
     function getQuote(
         uint amount,
         address base,
         address quote
     ) external view returns (uint out);
 
+    /// @notice Returns the bid and ask quotes for a given amount of base asset in quote asset.
+    /// @param amount The amount of base asset.
+    /// @param base The address of the base asset.
+    /// @param quote The address of the quote asset.
+    /// @return bidOut The bid quote amount in quote asset.
+    /// @return askOut The ask quote amount in quote asset.
     function getQuotes(
         uint amount,
         address base,
         address quote
     ) external view returns (uint bidOut, uint askOut);
 
+    /// @notice Returns the tick for a given amount of base asset in quote asset.
+    /// @param amount The amount of base asset.
+    /// @param base The address of the base asset.
+    /// @param quote The address of the quote asset.
+    /// @return tick The tick value.
     function getTick(
         uint amount,
         address base,
         address quote
     ) external view returns (uint tick);
 
+    /// @notice Returns the bid and ask ticks for a given amount of base asset in quote asset.
+    /// @param amount The amount of base asset.
+    /// @param base The address of the base asset.
+    /// @param quote The address of the quote asset.
+    /// @return bidTick The bid tick value.
+    /// @return askTick The ask tick value.
     function getTicks(
         uint amount,
         address base,
--- src/utils/CVCClient.sol
+++ src/utils/CVCClient.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.0;
 import "solmate/tokens/ERC20.sol";
 import "euler-cvc/interfaces/ICreditVaultConnector.sol";
 
+/// @title CVCClient
+/// @dev This contract is an abstract base contract for interacting with the Credit Vault Connector (CVC).
+/// It provides utility functions for authenticating callers in the context of the CVC,
+/// scheduling and forgiving status checks, and liquidating collateral shares.
 abstract contract CVCClient {
     ICVC private immutable cvc;
 
@@ -16,11 +20,14 @@ abstract contract CVCClient {
         cvc = _cvc;
     }
 
+    /// @notice Modifier to ensure that the function is only called by the CVC.
     modifier CVCOnly() {
         if (msg.sender != address(cvc)) revert NotAuthorized();
         _;
     }
 
+    /// @notice Authenticates the caller in the context of the CVC.
+    /// @return The address of the account on behalf of which the operation is being executed.
     function CVCAuthenticate() internal view returns (address) {
         if (msg.sender == address(cvc)) {
             (address onBehalfOfAccount, ) = cvc.getExecutionContext(address(0));
@@ -30,6 +37,9 @@ abstract contract CVCClient {
         return msg.sender;
     }
 
+    /// @notice Authenticates the caller for a borrow operation in the context of the CVC.
+    /// @dev Ensures that the vault is enabled as a controller for the account.
+    /// @return The address of the account on behalf of which the operation is being executed.
     function CVCAuthenticateForBorrow() internal view returns (address) {
         if (msg.sender == address(cvc)) {
             (address onBehalfOfAccount, bool controllerEnabled) = cvc
@@ -47,6 +57,9 @@ abstract contract CVCClient {
         return msg.sender;
     }
 
+    /// @notice Retrieves the owner of an account.
+    /// @param account The address of the account.
+    /// @return owner The address of the account owner.
     function getAccountOwner(
         address account
     ) internal view returns (address owner) {
@@ -57,12 +70,19 @@ abstract contract CVCClient {
         }
     }
 
+    /// @notice Retrieves the collaterals enabled for an account.
+    /// @param account The address of the account.
+    /// @return An array of addresses that are enabled collaterals for the account.
     function getCollaterals(
         address account
     ) internal view returns (address[] memory) {
         return cvc.getCollaterals(account);
     }
 
+    /// @notice Checks whether a vault is enabled as a collateral for an account.
+    /// @param account The address of the account.
+    /// @param vault The address of the vault.
+    /// @return A boolean value that indicates whether the vault is an enabled collateral for the account.
     function isCollateralEnabled(
         address account,
         address vault
@@ -70,12 +90,19 @@ abstract contract CVCClient {
         return cvc.isCollateralEnabled(account, vault);
     }
 
+    /// @notice Retrieves the controllers enabled for an account.
+    /// @param account The address of the account.
+    /// @return An array of addresses that are the enabled controllers for the account.
     function getControllers(
         address account
     ) internal view returns (address[] memory) {
         return cvc.getControllers(account);
     }
 
+    /// @notice Checks whether a vault is enabled as a controller for an account.
+    /// @param account The address of the account.
+    /// @param vault The address of the vault.
+    /// @return A boolean value that indicates whether the vault is an enabled controller for the account.
     function isControllerEnabled(
         address account,
         address vault
@@ -83,18 +110,25 @@ abstract contract CVCClient {
         return cvc.isControllerEnabled(account, vault);
     }
 
+    /// @notice Disables the calling contract as a controller for an account.
+    /// @param account The address of the account.
     function disableSelfAsController(address account) internal {
         cvc.disableController(account);
     }
 
+    /// @notice Schedules a status check for an account.
+    /// @param account The address of the account.
     function requireAccountStatusCheck(address account) internal {
         cvc.requireAccountStatusCheck(account);
     }
 
+    /// @notice Schedules a status check for the calling vault.
     function requireVaultStatusCheck() internal {
         cvc.requireVaultStatusCheck();
     }
 
+    /// @notice Schedules a status check for an account and the calling vault.
+    /// @param account The address of the account.
     function requireAccountAndVaultStatusCheck(address account) internal {
         if (account == address(0)) {
             cvc.requireVaultStatusCheck();
@@ -103,22 +137,35 @@ abstract contract CVCClient {
         }
     }
 
+    /// @notice Forgives a previously deferred account status check.
+    /// @dev Can only be called by the enabled controller of the account.
+    /// @param account The address of the account.
     function forgiveAccountStatusCheck(address account) internal {
         cvc.forgiveAccountStatusCheck(account);
     }
 
+    /// @notice Checks whether the status check is deferred for a given account.
+    /// @param account The address of the account.
+    /// @return A boolean flag that indicates whether the status check is deferred.
     function isAccountStatusCheckDeferred(
         address account
     ) internal view returns (bool) {
         return cvc.isAccountStatusCheckDeferred(account);
     }
 
+    /// @notice Liquidates a certain amount of collateral shares from a violator's vault.
+    /// @dev This function impersonates the violator and transfers the specified amount of shares from the violator's vault to the liquidator.
+    /// @param vault The address of the vault from which the shares are being liquidated.
+    /// @param violator The address of the violator whose shares are being liquidated.
+    /// @param liquidator The address to which the liquidated shares are being transferred.
+    /// @param shares The amount of shares to be liquidated.
     function liquidateCollateralShares(
         address vault,
         address violator,
         address liquidator,
         uint shares
     ) internal {
+        // Impersonate the violator to transfer shares from the violator's vault to the liquidator.
         (bool success, ) = cvc.impersonate(
             vault,
             violator,
